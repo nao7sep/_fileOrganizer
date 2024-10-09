d5added9607155f0458ec27be098cca142b90c91
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-10-09 14:33:11 +0900

    CreateWindow designed

---
+++ _fileOrganizer/CreateWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace _fileOrganizer
+{
+    /// <summary>
+    /// Interaction logic for CreateWindow.xaml
+    /// </summary>
+    public partial class CreateWindow: Window
+    {
+        public CreateWindow ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
