313e2327e09d8ae7cacd77287b4fbc1aa3010a83
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-10-10 17:13:36 +0900

    RenameWindow added; not tested

---
+++ _fileOrganizer/RenameWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace _fileOrganizer
+{
+    /// <summary>
+    /// Interaction logic for RenameWindow.xaml
+    /// </summary>
+    public partial class RenameWindow: Window
+    {
+        public RenameWindow ()
+        {
+            InitializeComponent ();
+        }
+
+        private void WindowLoaded (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NewNameTextBox.Focus ();
+            }
+
+            catch (Exception xException)
+            {
+                Utility.TryHandleException (this, xException);
+            }
+        }
+
+        private void NewNameTextBoxKeyDown (object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.Enter)
+                {
+                    var xViewModel = (RenameWindowViewModel) DataContext;
+
+                    if (xViewModel.CanCreate)
+                        CreateButtonClick (sender, e);
+                }
+
+                else if (e.Key == Key.Escape)
+                    CancelButtonClick (sender, e);
+            }
+
+            catch (Exception xException)
+            {
+                Utility.TryHandleException (this, xException);
+            }
+        }
+
+        private void NewNameTextBoxTextChanged (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var xViewModel = (RenameWindowViewModel) DataContext;
+                string? xTrimmedName = xViewModel.NewName?.Trim ();
+
+                if (string.IsNullOrWhiteSpace (xTrimmedName) == false)
+                {
+                    var xExistingNames = ((MainWindowViewModel) Owner.DataContext).Groups?.Select (x => x.Name);
+
+                    if (xExistingNames == null || xExistingNames.Contains (xTrimmedName, StringComparer.OrdinalIgnoreCase) == false)
+                    {
+                        xViewModel.ErrorMessage = null;
+                        xViewModel.CanCreate = true;
+                        return;
+                    }
+
+                    else xViewModel.ErrorMessage = "Name already exists.";
+                }
+
+                xViewModel.CanCreate = false;
+            }
+
+            catch (Exception xException)
+            {
+                Utility.TryHandleException (this, xException);
+            }
+        }
+
+        private void CreateButtonClick (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var xViewModel = (RenameWindowViewModel) DataContext;
+                xViewModel.IsCreated = true;
+                Close ();
+            }
+
+            catch (Exception xException)
+            {
+                Utility.TryHandleException (this, xException);
+            }
+        }
+
+        private void CancelButtonClick (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var xViewModel = (RenameWindowViewModel) DataContext;
+                xViewModel.IsCreated = false;
+                Close ();
+            }
+
+            catch (Exception xException)
+            {
+                Utility.TryHandleException (this, xException);
+            }
+        }
+    }
+}
---
+++ _fileOrganizer/RenameWindowViewModel.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+
+namespace _fileOrganizer
+{
+    public class RenameWindowViewModel: INotifyPropertyChanged
+    {
+        private string? _title;
+
+        public string? Title
+        {
+            get => _title;
+
+            set
+            {
+                if (string.Equals (_title, value, StringComparison.Ordinal) == false)
+                {
+                    _title = value;
+                    OnPropertyChanged (nameof (Title));
+                }
+            }
+        }
+
+        private string? _message;
+
+        public string? Message
+        {
+            get => _message;
+
+            set
+            {
+                if (string.Equals (_message, value, StringComparison.Ordinal) == false)
+                {
+                    _message = value;
+                    OnPropertyChanged (nameof (Message));
+                }
+            }
+        }
+
+        private string? _currentName;
+
+        public string? CurrentName
+        {
+            get => _currentName;
+
+            set
+            {
+                if (string.Equals (_currentName, value, StringComparison.Ordinal) == false)
+                {
+                    _currentName = value;
+                    OnPropertyChanged (nameof (CurrentName));
+                }
+            }
+        }
+
+        private string? _newName;
+
+        public string? NewName
+        {
+            get => _newName;
+
+            set
+            {
+                if (string.Equals (_newName, value, StringComparison.Ordinal) == false)
+                {
+                    _newName = value;
+                    OnPropertyChanged (nameof (NewName));
+                }
+            }
+        }
+
+        private string? _errorMessage;
+
+        public string? ErrorMessage
+        {
+            get => _errorMessage;
+
+            set
+            {
+                if (string.Equals (_errorMessage, value, StringComparison.Ordinal) == false)
+                {
+                    _errorMessage = value;
+                    OnPropertyChanged (nameof (ErrorMessage));
+                }
+            }
+        }
+
+        private bool _canCreate;
+
+        public bool CanCreate
+        {
+            get => _canCreate;
+
+            set
+            {
+                if (_canCreate != value)
+                {
+                    _canCreate = value;
+                    OnPropertyChanged (nameof (CanCreate));
+                }
+            }
+        }
+
+        public bool? IsCreated { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged (string propertyName) => PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+    }
+}
